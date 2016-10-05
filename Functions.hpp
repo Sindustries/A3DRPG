487730acb0665ff4770303903d71b3f034e37558
Sindustries <jleach92@hotmail.co.uk>
2016-10-05 19:41:17 +0100

    Initial Upload

---
+++ Functions.hpp
@@ -0,0 +1,17 @@
+class RPG_Core {
+	tag = "RPG";
+	class core {
+		file = "core\core";
+		class checkDist {};
+		class findPos {};
+		class getSetting {};
+		class getSettings {};
+	};
+	
+	class functions {
+		file = "core\functions";
+	};
+}
+	
+
+
